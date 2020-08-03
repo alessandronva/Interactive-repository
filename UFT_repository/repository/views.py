aa38394f9f77677bad0d52f85dd48e71039429f6
EgCode97 <eliasgerardoc@gmail.com>
2020-08-03 16:08:22 -0400

    query and print on console special mention projects and every tutor

--- UFT_repository/repository/views.py
+++ UFT_repository/repository/views.py
@@ -7,9 +7,13 @@ import django.http
 def home(request):
     context = dict()
     context['title'] = 'Home'
+    # query the projects tagged as special mention to show in home page
     context['highlighted'] = Project.objects.filter(special_mention=1)
 
+    # query all the tutors to show as filters
+    context['tutors'] = Tutor.objects.all()
     print(context['highlighted'])
+    print(context['tutors'])
 
-    
+    # TO DO: render home template    
     return django.http.HttpResponse(content=str(context))
