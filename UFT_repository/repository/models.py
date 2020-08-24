0d09d360ad81debda473e72b8da2609d35c92946
EgCode97 <eliasgerardoc@gmail.com>
2020-08-24 02:58:21 -0400

    updated models and database

--- UFT_repository/repository/models.py
+++ UFT_repository/repository/models.py
@@ -10,18 +10,16 @@ def validate_pdf(file):
         raise ValidationError(gettext_lazy('Debe ser un archivo PDF'))
 
 class Tutor(models.Model):
-    name = models.CharField(verbose_name="Nombre", max_length=20)
-    lastname = models.CharField(verbose_name="Apellido", max_length=20)
-
+    name = models.CharField(verbose_name="Nombre y Apellido", max_length=50)
     def __str__(self):
-        return f"{self.name} {self.lastname}"
+        return f"{self.name}"
 
 class Project(models.Model):
     title = models.TextField(verbose_name="Titulo", unique=True, max_length=300)
     description = models.TextField(verbose_name='Resumen')
     author = models.CharField(verbose_name='Autor', max_length=60)
     date = models.DateField(verbose_name="Fecha")
-    tutor = models.ForeignKey(to=Tutor, on_delete=models.DO_NOTHING)
+    tutor = models.ForeignKey(to=Tutor, on_delete=models.CASCADE)
     special_mention = models.BooleanField(verbose_name="Mencion especial")
     file = models.FileField(verbose_name="Archivo PDF", upload_to='./files', validators=(validate_pdf,))
 
