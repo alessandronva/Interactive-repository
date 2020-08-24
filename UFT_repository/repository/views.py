e99d4e248e87e9141ca5703a24ea095aba3afaaf
EgCode97 <eliasgerardoc@gmail.com>
2020-08-24 03:00:20 -0400

    created the 'search' view wich read the form data and gets the projets from DB

--- UFT_repository/repository/views.py
+++ UFT_repository/repository/views.py
@@ -2,13 +2,29 @@ from django.shortcuts import render
 from .models import Project, Tutor
 from UFT_repository import settings
 
+import datetime
 import django.http
+
+
+def get_search_years():
+    query = Project.objects.all().order_by('date')
+    min, max = query.first().date.year, query.last().date.year
+
+    print("AÑOS -->", min, max)
+
+    return [year for year in range(min, max + 1)]
+
+def get_tutors():
+    return Tutor.objects.all() 
 
 # Create your views here.
 def home(request):
     context = dict()
     context['title'] = 'Inicio'
     context['style_table'] = True
+    context['years'] = get_search_years()
+    context['tutors'] = Tutor.objects.all()
+
     # query the projects tagged as special mention to show in home page
     context['projects'] = list(enumerate(Project.objects.filter(special_mention=1), 1))[:10]
 
@@ -19,14 +35,65 @@ def home(request):
     print(context['projects'])
     print(context['tutors'])
 
-
-    # TO DO: render home template    
     return render(request, "home.html", context)
-    #return django.http.HttpResponse(content=str(context))
+
+def search(request):
+    context = dict()
+    context['style_table'] = True
+    context['in_search'] = True
+    context['years'] = get_search_years()
+    context['tutors'] = Tutor.objects.all()
+
+    form_data = dict(request.GET) # dict
+    print(form_data)
+
+    # form data values are a list containing a str with the form value
+    # take the value as a str and not a list
+    for key in form_data:
+        form_data[key] = form_data[key][0]
+
+    # "spec-mention" appears on the request if selected with the value of "on"
+    # take "on" as True, if it doesnt appears it wasn't checked, take as False
+    if "spec-mention" in form_data:
+        form_data["spec-mention"] = True
+    else:
+        form_data["spec-mention"] = False
+
+    context['title'] = form_data['title']
+
+    # Query database and filter by the title with a NO CASE SENSITIVE SEARCH
+    # if no title was searched get all the projects
+    if not form_data['title']:
+        query = Project.objects.all()
+    else:
+        query = Project.objects.filter(title__icontains=form_data['title'])
+
+    # if a tutor was selected get the projects associated with the given tutor
+    if form_data['tutor']:
+        query = query.filter(tutor__name__exact = form_data['tutor'])
+
+    # get the projects between "year-low" and "year-high"
+    query = query.filter(
+        date__gte= datetime.date(int(form_data['year-low']), 1, 1),
+        date__lte= datetime.date(int(form_data['year-high']), 12, 31)
+    )
+
+    # Remove special-mentions projects if the option was unchecked 
+    if not form_data['spec-mention']:
+        query = query.filter(special_mention = False)
+
+    print("\n\nBUSQUEDA\n\n", query)
+    context['projects'] = list(enumerate(query, 1))
+
+    return render(request, "home.html", context)
+    #return django.http.HttpResponse(query)
 
 def show_project(request, title):
     context = dict()
     context['title'] = 'Ver proyecto'
+    context['years'] = get_search_years()
+    context['tutors'] = Tutor.objects.all()
+
     query = Project.objects.get(title=title)
 
     context['title'] = title
