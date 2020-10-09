16666728c9328ac34a02d0885574a08348db286d
EgCode97 <eliasgerardoc@gmail.com>
2020-10-08 20:37:10 -0400

    search problem from about us page fixed

--- UFT_repository/info/views.py
+++ UFT_repository/info/views.py
@@ -4,12 +4,16 @@ from django.http import HttpResponse
 from UFT_repository.settings import BASE_DIR
 
 from .models import Employee, Contact
+from repository.views import get_search_years
+from repository.models import Tutor
 
 # Create your views here.
 def about(request):
     context = dict()
     context['title'] = 'Sobre nosotros'
     context['style_about'] = True
+    context['years'] = get_search_years()
+    context['tutors'] = Tutor.objects.all()
 
     query = Employee.objects.all().filter(status=True)
 
